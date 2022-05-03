730fe6d0560e73f00f5354f49543e16540abaef7
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-05-03 07:58:15 -0400

    updating securitygroup for outbound access

--- security_groups.tf
+++ security_groups.tf
@@ -3,21 +3,21 @@ resource "aws_security_group" "demo_servers_sg" {
 
   #INBOUND CONNECTIONS 
   ingress {
-    description     = "Allows SSH connection from the Bastion Host"
+    description = "Allows SSH connection from the Bastion Host"
     #security_groups = ["sg-037583cd48f4cd97d"]
     cidr_blocks = ["192.168.0.0/16"]
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
   }
- 
+
   #OUTBOUND CONNECTIONS 
   egress {
-    description = "Allows connection to Logstash" #send info to Logstash
-    cidr_blocks = ["192.168.0.0/16"]
-    from_port   = 5044
-    protocol    = "tcp"
-    to_port     = 5044
+    description = "Allows outbound connection"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
   }
   tags = {
     Name = "demo_servers_sg"
